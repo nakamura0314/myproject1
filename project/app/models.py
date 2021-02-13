fa31ca3ed47bf5830bce709d0ad5a88faf7fd307
中村渉帆 <takaho@Takaho-Nakamura.local>
2021-02-13 22:36:40 +0900

    makemigrationsまだ

--- project/app/models.py
+++ project/app/models.py
@@ -1,7 +1,16 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class cost(models.Model):
-    """その日の費用、今はスケジュールとして扱う"""
+    """その日の費用、naritoではスケジュールとして扱われている"""
+    summary = models.CharField('概要', max_length=30)
+    money = models.IntegerField(
+        '金額', validators=[MinValueValidator(0), MaxValueValidator(100000000)])
+    description = models.TextField('詳細な説明', blank=True)
+    created_at = models.DateTimeField('日付', default=timezone.now)
+
+    def __str__(self):
+        return self.summary
